4f5485813a61335810469c4db1d5d1d4539a291d
liuyu <1006216705@qq.com>
2020-01-03 15:57:32 +0800

    数组实现 栈 实现 带括号的表达式计算

--- src/stack/ExpressionStack.java
+++ src/stack/ExpressionStack.java
@@ -7,18 +7,33 @@ package stack;
  */
 public class ExpressionStack {
     public static void main(String[] args) {
+        
+        //运算符栈 当前场景，最大4个
         ArrayStack2 operStack = new ArrayStack2(10);
+        //数栈 当前场景，最大4个
         ArrayStack2 numStack = new ArrayStack2(10);
         
-        String expression = "700*2-30+3-40";
+        //String expression = "70*2-(3-30*4+80-90)";//267
+        // String expression = "3-7*2+3";//-8
+        //运算表达式
+        String expression = "3+(3+3)-3*7+3-(4+3*2)+(3*4)";//-7
         
         //遍历表达式
         //1、若是数字，入数栈
         //2、若是符号，先判断符号栈是否为空
         //2.1、为空，直接入符号栈
-        //2.2、不为空，取符号栈顶，判断当前符号与栈顶符号的优先级，若小，数栈弹出两个数，符号栈弹出一个符号进行计算
-        //并把计算结果入数栈，再把运算符入符号栈，若大，直接入符号栈
-        //当符号栈为空，说明整个表达式计算结束，取出数栈中的数（此时数栈只有一个数据），为表达式结果
+        //2.2、不为空
+        //2.2.1、若是左括号，直接入符号栈
+        //2.2.2、若是右括号，
+        //1）从数栈弹出两个数，符号栈弹出一个符号进行计算，并把计算结果入数栈，
+        //2）反复上述操作，直到符号栈顶为左括号，最后将左括号出栈
+        //2.2.3、若是其他运算符号
+        //1）取符号栈顶，判断当前符号与栈顶符号的优先级，
+        //2)若大，直接入符号栈
+        //3）若小，数栈弹出两个数，符号栈弹出一个符号进行计算并把计算结果入数栈，再把运算符入符号栈
+        //3）反复上述操作，直到符号栈顶为左括号或符号栈为空，最后将当前的运算符入栈
+        //3、当表达式遍历完毕，在当符号栈不为空的前提下，反复做数栈弹出两个数，符号栈弹出一个符号进行计算并把计算结果入数栈
+        //4、在当符号栈为空，说明整个表达式计算结束，取出数栈中的数（此时数栈只有一个数据），为表达式结果
         
         int index = 0;
         int result = 0 ;
@@ -32,17 +47,38 @@ public class ExpressionStack {
                 //是运算符
                 if(!operStack.isEmpty()){
                     //运算符栈不为空
+                    if(operStack.isOpen(ch)){
+                        //是左括号，直接入运算符栈
+                        operStack.push(ch);
+                    }else if(operStack.isClose(ch)){
+                        //是右括号，反复从数栈弹出两个数，符号栈弹出一个符号进行计算，并把计算结果入数栈，找到最近的(为止
+                        while (!operStack.isOpen(operStack.peek())){
+                            num1 = numStack.pop();
+                            num2 = numStack.pop();
+                            result = numStack.operation(num1,num2,operStack.pop());
+                            numStack.push(result);
+                        }
+                        //最后将左括号出栈
+                        operStack.pop();
+                    }
                     //判断当前符号与栈顶符号的优先级
-                    if(operStack.operPriority(ch) > operStack.operPriority(operStack.peek())){
+                    else if(operStack.operPriority(ch) > operStack.operPriority(operStack.peek())){
                         //当前符号 大，直接入符号栈
                         operStack.push(ch);
                     }else{
                         //当前符号 小
-                        //数栈弹出两个数，符号栈弹出一个符号进行计算
-                        num1 = numStack.pop();
-                        num2 = numStack.pop();
-                        result = numStack.operation(num1,num2,operStack.pop());
-                        numStack.push(result);
+                        //反复 数栈弹出两个数，符号栈弹出一个符号进行计算并把计算结果入数栈，直到符号栈栈顶为（，或 符号栈为空为止
+                        while (!operStack.isEmpty()){
+                            if(!operStack.isOpen(operStack.peek())){
+                                num1 = numStack.pop();
+                                num2 = numStack.pop();
+                                result = numStack.operation(num1,num2,operStack.pop());
+                                numStack.push(result);
+                            }else{
+                                break;
+                            }
+                        }
+                        //再把当前运算符ch的值入符号栈
                         operStack.push(ch);
                     }
                 }else{
@@ -83,7 +119,7 @@ public class ExpressionStack {
             }
         }
         
-        //如果已遍历完
+        //如果已遍历完表达式，再计算
         while(!operStack.isEmpty()){
             num1 = numStack.pop();
             num2 = numStack.pop();
@@ -173,7 +209,8 @@ class ArrayStack2 {
     
     //判断是否是运算符号
     public boolean isOper(int oper){
-        return '+' == oper || '-' == oper || '*' == oper || '/' == oper;
+        return '+' == oper || '-' == oper || '*' == oper || '/' == oper
+                || '(' == oper || ')' == oper;
     }
     
     //为+ - * /确定等级
@@ -185,6 +222,14 @@ class ArrayStack2 {
         }else{
             return -1;
         }
+    }
+    
+    public boolean isOpen(int oper){
+        return '(' == oper;
+    }
+    
+    public boolean isClose(int oper){
+        return ')' == oper;
     }
     
     //计算
